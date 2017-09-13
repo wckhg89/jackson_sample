b7155ea4406a3375f8669f54343bbf9f6614fa1d
kanghonggu <wckhg89@gmail.com>
2017-09-13 16:02:29 +0900

    lombok setting.

--- src/main/java/com/guppy/domain/Student.java
+++ src/main/java/com/guppy/domain/Student.java
@@ -3,11 +3,14 @@ package com.guppy.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.ToString;
 
 import java.util.List;
 
+@ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
+
 
     @JsonProperty("id")
     private String id;
@@ -21,6 +24,7 @@ public class Student {
     @JsonProperty("subjectList")
     private List<Subject> subjectList;
 
+    @ToString
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Subject {
 
